aa57dc4aa195ebc94cb00f3e746de36bbfc72a31
Gaetano Sabino <gaetano@baseweb.space>
2021-04-15 11:43:34 +0200

    new service to users

--- src/app/modules/search-users/services/search-user.service.ts
+++ src/app/modules/search-users/services/search-user.service.ts
@@ -1,9 +1,27 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {environment} from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchUserService {
 
-  constructor() { }
+  private _urlUser = `${environment.urlService}/users`;
+
+  constructor(
+    readonly http: HttpClient
+  ) {
+  }
+
+  getAllUser(): Observable<any> {
+
+    return this.http.get(
+      this._urlUser,
+      {
+        observe: 'response',
+      }
+    );
+  }
 }
