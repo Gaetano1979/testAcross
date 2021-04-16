5182f1b32bf9158a8e22bed4ed3793440a88edd0
Gaetano Sabino <gaetano@baseweb.space>
2021-04-16 13:12:48 +0200

    create a new modal service

--- src/app/modules/search-users/services/modal.service.ts
+++ src/app/modules/search-users/services/modal.service.ts
@@ -1,9 +1,28 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import Swal, {SweetAlertIcon, SweetAlertResult} from 'sweetalert2';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ModalService {
 
-  constructor() { }
+  constructor() {
+  }
+
+  getAlert(title: string, icon: SweetAlertIcon): void {
+    Swal.fire({
+      title,
+      icon
+    });
+  }
+
+  getModalWithPromise(): Promise<SweetAlertResult<any>> {
+    return Swal.fire({
+      title: 'Do you want to save the changes?',
+      showDenyButton: true,
+      showCancelButton: true,
+      confirmButtonText: `Save`,
+      denyButtonText: `Don't save`,
+    });
+  }
 }
