40b70c613dd31369ecfbc2e4585e4a86fc1d10a0
Gaetano Sabino <gaetano@baseweb.space>
2021-04-16 13:13:52 +0200

    added ricerca e elimina search component

--- src/app/modules/search-users/search-users.component.ts
+++ src/app/modules/search-users/search-users.component.ts
@@ -1,7 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 import {Title} from '@angular/platform-browser';
 import {SearchUserService} from './services/search-user.service';
-import {Observable} from 'rxjs';
+import {catchError, map, switchMap, tap} from 'rxjs/operators';
+import {IUserResponse} from '../../models/user-response.interface';
+import {Observable, of, Subject} from 'rxjs';
+import {ModalService} from './services/modal.service';
+import {NgxSpinnerService} from 'ngx-spinner';
 
 @Component({
   selector: 'app-search-users',
@@ -10,12 +14,16 @@ import {Observable} from 'rxjs';
 })
 export class SearchUsersComponent implements OnInit {
   filtro_title = 'Filtro Ricerca User';
+  formRicerca: string[] = [];
 
-
+  users$: Observable<IUserResponse[] | null> = new Subject();
 
 
   constructor(
     readonly title: Title,
+    readonly searchService: SearchUserService,
+    readonly modal: ModalService,
+    readonly spinner: NgxSpinnerService
   ) {
     this.title.setTitle('Ricerca User');
   }
@@ -25,5 +33,55 @@ export class SearchUsersComponent implements OnInit {
   }
 
 
+  ricerca({full_name, email, telefono}: IUserResponse): void {
+    if (!!full_name) {
+      this.users$ = this.searchService.getAllUser(full_name).pipe(
+        tap(response => {
+          console.log(response);
+          if (!!response && response.length === 0) {
+            this.modal.getAlert('Nessun risultato trovato', 'info');
+          }else if (!!response && response.length > 0){
+            this.modal.getAlert('Risultati ricevuti', 'success');
+          }
+        })
+      );
 
+    } else {
+      this.users$ = this.searchService.getAllUser().pipe(
+        tap(response => {
+          console.log(response);
+          if (!!response && response.length > 0) {
+            this.modal.getAlert('Risultati ricevuti', 'success');
+          } else if (!!response && response.length === 0) {
+            this.modal.getAlert('Nessun risultato trovato', 'info');
+          }
+        })
+      );
+    }
+
+  }
+
+  elimina(id: string): void {
+    this.modal.getModalWithPromise().then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        if (!!id) {
+          this.users$ = this.searchService.deleteUser(id).pipe(
+            switchMap(user => this.searchService.getAllUser().pipe(
+              map(response => ({response, user})),
+              catchError(error => of({
+                response: null,
+                user: {id: null, telefono: null, email: null, full_name: null, createdAt: null}
+              }))
+            )),
+            tap(response => this.modal.getAlert('Utente Eliminato', 'success')),
+            map(response => response.response)
+          );
+        }
+      } else if (result.isDenied) {
+        this.modal.getAlert('Nessuna Eliminazione', 'info');
+      }
+    });
+
+  }
 }
