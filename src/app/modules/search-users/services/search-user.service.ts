40ef13c5f433f906b2cff8a0ba569edeb88af5f3
Gaetano Sabino <gaetano@baseweb.space>
2021-04-16 13:13:04 +0200

    create search service

--- src/app/modules/search-users/services/search-user.service.ts
+++ src/app/modules/search-users/services/search-user.service.ts
@@ -1,7 +1,9 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpParams} from '@angular/common/http';
+import {HttpClient, HttpParams, HttpResponse} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {environment} from '../../../../environments/environment';
+import {IUserResponse} from '../../../models/user-response.interface';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -15,13 +17,52 @@ export class SearchUserService {
   ) {
   }
 
-  getAllUser(): Observable<any> {
+  private getAllUserHttpResponse(filtro?: string): Observable<HttpResponse<IUserResponse[]>> {
+    let params: HttpParams = new HttpParams();
 
-    return this.http.get(
+    if (!!filtro) {
+      params = params.set('search', filtro);
+    }
+
+    return this.http.get<IUserResponse[]>(
       this._urlUser,
       {
         observe: 'response',
+        params
       }
     );
   }
+
+
+  getAllUser(filtro?: string): Observable<IUserResponse[] | null> {
+    return this.getAllUserHttpResponse(filtro).pipe(
+      map(response => response.body)
+    );
+  }
+
+  getUserId(id: string): Observable<IUserResponse | null> {
+    return this.http.get<IUserResponse>(`${this._urlUser}/${id}`, {observe: 'response'}).pipe(
+      map(response => response.body)
+    );
+  }
+
+  postNewUser(user: IUserResponse): Observable<IUserResponse | null> {
+    return this.http.post<IUserResponse>(`${this._urlUser}`, user, {observe: 'response'}).pipe(
+      map(response => response.body)
+    );
+  }
+
+  setUser(user: IUserResponse): Observable<IUserResponse | null> {
+    return this.http.put<IUserResponse>(`${this._urlUser}/${user.id}`, user, {observe: 'response'}).pipe(
+      map(response => response.body)
+    );
+  }
+
+  deleteUser(id: string): Observable<IUserResponse | null> {
+    return this.http.delete<IUserResponse>(`${this._urlUser}/${id}`, {observe: 'response'}).pipe(
+      map(response => response.body)
+    );
+  }
+
+
 }
